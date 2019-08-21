780f9020448eba4202688dc1c01406447be6e95a
tardyparty <tardyparty@users.noreply.github.com>
2019-08-20 20:38:53 -0500

    restructure js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,151 +1,169 @@
-
-
-// You'll create a trivia game that shows only one question until the player answers it or their time runs out.
-
-
-// If the player selects the correct answer, show a screen congratulating them for choosing the right option. After a few seconds, display the next question -- do this without user input.
-
-
-// The scenario is similar for wrong answers and time-outs.
-
-
-// If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
-
-
-// If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
-
-
-// On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
-
-
-var questions = [
+var quiz = [
     {
-        Q: "Ray Charles famously had this state on his mind. What is its capital?",
+        question: "Ray Charles famously had this state on his mind. What is its capital?",
+        options: ["Atlanta", "New York", "Los Angeles", "Austin"],
+        correctAnswer: "Atlanta",
+        img: "assets/images/atlanta.jpg" 
     },
     {
-        Q: "Okay, now, you're not an all-star of the NBA, but you did get your game on when you won the NBA's Sixth Man of the Year award in 2011. Who are you?",
+        question: "Okay, now, you're not an all-star of the NBA, but you did get your game on when you won the NBA's Sixth Man of the Year award in 2011. Who are you?",
+        options: ["Michael Jordan", "Magic Johnson", "Lamar Odom", "Larry Bird",],
+        correctAnswer: "Lamar Odom",
+        img: "assets/images/lamarOdom.jpg"
     },
     {
-        Q: "Who was the relatively unknown patent clerk who discovered that energy equals mass times the speed of light squared?",
-    },
-        {Q: "On a square-rigged ship, the sale set furthest forward is called what?",
-    },
-    {
-        Q: "According to a recent survey, this is the most common learning disability among American adolescents.",
+        question: "Who was the relatively unknown patent clerk who discovered that energy equals mass times the speed of light squared?",
+        options: ["Thomas Edison", "Albert Einstein", "Alexander Graham Bell", "Henry Ford"],
+        correctAnswer: "Albert Einstein",
+        img: "assets/images/albert.jpg"
     },
     {
-        Q: "The standard American analog scale has a maximum capacity of what weight?",
+        question: "On a square-rigged ship, the sale set furthest forward is called what?",
+        options: ["Flying Jib", "Spinnaker", "Genoa", "Main Sail"],
+        correctAnswer: "Flying Jib",
+        img: "assets/images/flyingJib.jpg"
     },
     {
-        Q: "This 2001 masterpiece from Gilles Paquet-Brenner explores the intricate dynamics of a family in disarray.",
+        question: "According to a recent survey, this is the most common learning disability among American adolescents.",
+        options: ["ADD", "Dislexia", "Illiteracy", "ADHD"],
+        correctAnswer: "Dislexia",
+        img: "assets/images/dislexia.jpeg"
+    },
+    {
+        question: "The standard American analog scale has a maximum capacity of what weight?",
+        options: ["250lbs", "300lbs", "350lbs", "500lbs"],
+        correctAnswer: "300lbs",
+        img: "assets/images/300lb.jpg"
+    },
+    {
+        question: "This 2001 masterpiece from Gilles Paquet-Brenner explores the intricate dynamics of a family in disarray.",
+        options: ["Sarah's Key", "Crooked House", "Walled In", "Les Jolie Choses"],
+        correctAnswer: "Les Jolie Choses",
+        img: "assets/images/lesJolie.jpg"
     }  
 ];
 
-// the correct answer is randomized manually in answers array
-var answers = [
-    "Atlanta", "New York", "Los Angeles", "Austin",
-    "Michael Jordan", "Magic Johnson", "Lamar Odom", "Larry Bird",
-    "Thomas Edison", "Albert Einstein", "Alexander Graham Bell", "Henry Ford",
-    "Flying Jib", "Spinnaker", "Genoa", "Main Sail",
-    "250lbs", "300lbs", "350lbs", "500lbs",
-    "Dark Places", "Walled In", "Sarah's Key", "Les Jolies Choses"
-]
+// global variables
+let correct = 0;
+let incorrect = 0;
+let unanswered = 0;
+let counter = 5;
+let smallCounter = 5;
+let index = 0;
+let timer;
+let smalltimer;
 
-// scoring
-var correct = 0;
-var incorrect = 0;
-var unanswered = 0;
+// loads the question and answer page 
+function displayQuestion(){
+    counter = 5;
+    timer = setInterval(countDown, 1000);
 
-// display
-var currentQuestionIndex = 0;
-var answerIndex = 0;
-var finished = false;
-var userAnswer = '';
+    const question = quiz[index].question;
+    const options = quiz[index].options;
 
-
-var playGame = function(){
-
-    // press start and a new screen appears showing the first question
-    $("#start").on("click", function(){
-        $("#start").hide();
-        $(".box").show();
-        $(".answer").show();
-    })
-
-    countdown();
+    $("#timer").html(counter);
+    $("#game").html(`
+    <h1 class="box">${question}</h1>
+    ${displayAnswer(options)}
+    `);
 }
 
-var countdown = function(){
+// creates a button for each answer option 
+function displayAnswer(options){
+    let result = '';
 
-    displayQuestion();
-
-    var timer = 30;
-    setInterval(function(){
-        $("#timer").text(timer);
-        timer--;
-    }, 1000)
-
-    if (timer === 0){
-        // the same code goes here as if they got the answer wrong
+    for (var i=0; i< options.length; i++){
+        result += `<button class="answer" data-answer="${options[i]}">${options[i]}</button>`; 
     }
+
+    return result;
 }
 
-var displayQuestion = function(){
-    // reset userAnswer before each quesiton
-    userAnswer = '';
+// assesses if quiz is finished and proceeds to the next one
+function nextQuestion(){
+    // reveal game div
+    $("#game").show();
+    $("#timer").show();
+    $("#answerPage").hide();
 
-    // displays q and a into html
-    $("#question").text(questions[currentQuestionIndex].Q);
+    const isFinished = (quiz.length - 1) === index;
 
-    // answers are ++ after each display to go through all 4 possibilities
-    $("#answer1").text(answers[answerIndex]);
-    answerIndex++;
-    $("#answer2").text(answers[answerIndex]);
-    answerIndex++;
-    $("#answer3").text(answers[answerIndex]);
-    answerIndex++;
-    $("#answer4").text(answers[answerIndex]);
-    answerIndex++;
-
-    acceptAnswer();
-}
-
-var acceptAnswer = function(){
-    // start timer for 4 seconds before calling displayQuestion
-
-    currentQuestionIndex++;
-
-    $("answer1").on("click", function(){
-        userAnswer = $("#answer1").html();
-    });
-    $("answer2").on("click", function(){
-        userAnswer = $("#answer2").html();
-    });
-    $("answer3").on("click", function(){
-        userAnswer = $("#answer3").html();
-    });
-    $("answer4").on("click", function(){
-        userAnswer = $("#answer4").html();
-    });
-
-    // checks if answer was correct and displays correct page
-    if (userAnswer === "Atlanta" || userAnswer === "Lamar Odom" || userAnswer === "Albert Einstein" || userAnswer === "Flying Jib"
-    || userAnswer === "300lbs" || userAnswer === "Les Jolies Choses"){
-        wins++;
-        // create html divs for answer image and "Correct!"
+    if (isFinished){
+        // todo
+        displayResult();
     }
     else {
-        losses--;
-        // display worng answer page 
+        index++;
+        displayQuestion();
     }
-    
-    displayQuestion();
 }
 
-// creat an objcet for the images and text to be displayed for each answer.
+function timeUp(){
+    clearInterval(timer);
+    unanswered++;
+    // answerPage();
+}
 
-// the timer will start counting down first
+function countDown(){
+    counter--;
 
-// as soon as a button is clicked, the question will be judged right or wrong and reveal the true answer
+    $("#timer").html(counter);
 
-// after a few seconds, the next question will pop up
+    if (counter === 0) {
+        timeUp();
+    }
+}
+
+$(document).on("click", ".answer", function(){
+    clearInterval(timer);
+    const userAnswer = $(this).attr("data-answer");
+    const correctAnswer = quiz[index].correctAnswer;
+
+    if(userAnswer === correctAnswer){
+        correct++;
+        // nextQuestion();
+        correctPage();
+    }
+    else{
+        incorrect++;
+        // nextQuestion();
+        wrongPage();
+    }
+});
+
+function correctPage(){
+    const nextPage = `
+    <div class="correctAnswer">The Correct Answer is: ${quiz[index].correctAnswer}</div>
+    <img class="correctImg" src="${quiz[index].img}"> 
+    `
+    // hides the last quesiton
+    $("#game").hide();
+
+    // stop quesitons timer
+    clearInterval(timer);
+    $("#timer").hide();
+
+    // shows the correct answer and images 
+    $("#answerPage").html(nextPage);
+
+    // starts a 5 second clock before proceeding to next question
+    // smalltimer = setInterval(smallerCountDown, 5 * 1000);
+}
+
+// function smallerCountDown(){
+//     smallCounter = 5;
+//     smallCounter--;
+//     if(smallCounter === 0){
+//         nextQuestion();
+//     }
+// }
+
+function displayResult(){
+    const result = `
+    <p>Correct: ${correct}</p>
+    <p>Incorrect: ${incorrect}</p>
+    <button>Play Again</button>
+    `
+
+    $("#game").html(result);
+}
